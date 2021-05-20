47b8b9360b76cc7d08c191c5569cea2251a3336f
Mateo W. Racca <52387817+raccamateo@users.noreply.github.com>
2021-05-19 23:03:34 -0300

    Update elevaciones_split.R

--- elevaciones_split.R
+++ elevaciones_split.R
@@ -14,12 +14,16 @@ elevaciones_NY <- st_read("nyc_elevations_complete.shp") %>%
   st_as_sf()
 # el dataset original cuenta con 1.473.788 puntos
 
+# filtramos solo los puntos de elevación correspondientes a edificios
+elevaciones_NY_b <- elevaciones_NY %>%
+  filter(FEAT_CODE == 3020)
+
 # vamos a crear un nuevo dataframe con menos puntos para trabajar de manera más sencilla 
-sample_size = floor(0.01*nrow(elevaciones_NY))
-# floor 0.01 significa que vamos a seleccionar el 1% del total de puntos, es decir 14.737
+sample_size = floor(0.01*nrow(elevaciones_NY_b))
+# floor 0.01 significa que vamos a seleccionar el 1% del total de puntos, es decir 10.849
 
 #usamos set.seed para que en caso de querer replicar el muestreo esto sea posible
-set.seed(999)
+set.seed(777)
 picked = sample(seq_len(nrow(elevaciones_NY)),size = sample_size)
 
 #creamos el nuevo dataset
@@ -27,8 +31,8 @@ elevaciones_NY_muestra = elevaciones_NY[picked,]
 
 #para poder calcular semivariogramas eliminamos los valores de elevaciones negativos
 elevaciones_NY_muestra <- elevaciones_NY_muestra %>%
-  filter(ELEVATION > 0)
-
+  filter(ELEVATION > 0) 
+  
 #guardamos los datos como csv
 write.csv2(elevaciones_NY_muestra, file = "elevaciones_NY_muestra.csv")
 
